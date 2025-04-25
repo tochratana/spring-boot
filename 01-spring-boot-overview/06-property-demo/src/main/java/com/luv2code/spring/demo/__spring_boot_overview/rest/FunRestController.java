b0e4458eb060ce66d6f2c0b105aff5c4822c7ab4
RML-Cambo <ltomusic69@gmail.com>
2025-04-25 14:34:10 +0700

    overview

--- 01-spring-boot-overview/06-property-demo/src/main/java/com/luv2code/spring/demo/__spring_boot_overview/rest/FunRestController.java
+++ 01-spring-boot-overview/06-property-demo/src/main/java/com/luv2code/spring/demo/__spring_boot_overview/rest/FunRestController.java
@@ -1,11 +1,30 @@
 package com.luv2code.spring.demo.__spring_boot_overview.rest;
 
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class FunRestController {
+
+    // Inject property for: coach.name and team.name
+    @Value("${coach.name}")
+    private String coachName;
+    @Value("${team.name}")
+    private String teamName;
+
+    // expose endpoint for coach name
+    @GetMapping("/coachname")
+    public String getCoachName(){
+        return coachName;
+    }
+
+    // expose endpoint for team name
+    @GetMapping("teamname")
+    public String getTeamName(){
+        return teamName;
+    }
     // expose "/" that return "Hello World"
     @GetMapping("/")
     public String sayHello(){
